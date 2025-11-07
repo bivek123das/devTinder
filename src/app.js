002c2c78cea9f4130243bf6416cbcfacf7e5119a
Bivek Das <your.email@example.com>
2025-11-07 14:16:14 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -5,18 +5,39 @@ const app = express();
 
 // handling the request and send response
 
+// this route only handle the GET call  to /user
+app.use("/user",(req,res)=>{
+    res.send("HAHAHAHAHAHA")
+}
+)
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Bivek",lastName:"Das"}); 
+})
+
+app.post("/user",(req,res)=>{
+    //saving data to DB
+    res.send("Data successfully save to the database!"); 
+})
+
+app.delete("/user",(req,res)=>{
+    //saving data to DB
+    res.send("Deleted successfully!"); 
+})
+
+// this will match all the HTTP Method API calls to /test
 app.use("/test",(req,res)=>{
 res.send("Hello from the server");
 });
 
-app.use("/namaste", (req, res) => {
-    res.send("namaste!!");
-});
+// app.use("/namaste", (req, res) => {
+//     res.send("namaste!!");
+// });
 
-app.use('/',(req,res)=>{
-    res.send("HelloWorld from the Dashboard!");
-});
+// app.use('/',(req,res)=>{
+//     res.send("HelloWorld from the Dashboard!");
+// });
+
 // listen the port 
-app.listen(3000, ()=>{
+app.listen(7777, ()=>{
     console.log("Server is successfully listening on port 3000");
 });
