879112c2cc85909831c35b5b1ecdf2f7ef7a7324
Bivek Das <your.email@example.com>
2025-11-10 12:12:38 +0530

    wildcart-error-handling

--- src/app.js
+++ src/app.js
@@ -2,22 +2,28 @@ const express = require('express');
 
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth");
+app.use("/",(err,req,res,next)=>{
+  if(err){
+   // Log your error
+   res.status(500).send("Something went wrong!!!");
+  }
+});
+
+app.get("/getUserData",(req,res)=>{
+  try{
+    // Logic of DB call and get user data
+
+    throw new Error("wwhwbw");
+    res.send("User data send");
+  }
+  catch(err){
+    res.status(500).send("some error contact support team");
+  }
+
+});
 
 
-app.use("/admin", adminAuth);
 
-app.get("/user",userAuth, (req,res)=>{
-  res.send("User Data Sent");
-})
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All Data Sent");
-})
-
-app.get("/admin/deletedUser",(req,res)=>{
-   res.send("Deleted User");
-})
 
 // listen the port 
 app.listen(7777, ()=>{
